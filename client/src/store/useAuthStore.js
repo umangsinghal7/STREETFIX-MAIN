6e9c1fb29be7be469c084b16ac2579167a79c743
Umang <umangsinghal2007@gmail.com>
2026-06-12 20:51:00 +0530

    Fix auth API URL

--- client/src/store/useAuthStore.js
+++ client/src/store/useAuthStore.js
@@ -64,7 +64,7 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const API = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API = import.meta.env.VITE_API_URL || "https://streetfix-main.onrender.com";
 
 const useAuthStore = create((set, get) => ({
   user: null,

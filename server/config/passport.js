afc0e9ea9ea9cee90153c652f6dac98a462e6c7a
Umang <umangsinghal2007@gmail.com>
2026-06-12 19:32:19 +0530

    Fix Google OAuth callback URL

--- server/config/passport.js
+++ server/config/passport.js
@@ -7,7 +7,7 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: "http://localhost:5000/api/auth/google/callback",
+            callbackURL: "https://streetfix-main.onrender.com/api/auth/google/callback",
             passReqToCallback: false,
         },
         async (_accessToken, _refreshToken, profile, done) => {
